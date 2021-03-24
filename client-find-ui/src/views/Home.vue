847beb65b76f8d7c36dcc51aea4644b89c22c371
zhouhuafei <1123486116@qq.com>
2021-03-24 12:16:00 +0800

    feat: 🎸 404

--- client-find-ui/src/views/Home.vue
+++ client-find-ui/src/views/Home.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="home">
+    <router-link :to="{name: 'Log'}"><h1>Go to the log page.</h1></router-link>
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
