b17193b2a32105d75a780cf3793e02f4d408ce49
zhouhuafei <1123486116@qq.com>
2021-03-31 23:10:13 +0800

    feat: 🎸 find

--- api-service/app/service/log.js
+++ api-service/app/service/log.js
@@ -6,9 +6,36 @@ class LogService extends Service {
     const query = this.ctx.request.query
     const limit = +query.limit || 10
     const page = +query.page || 1
-    const count = await this.ctx.model.Log.count()
-    const list = await this.ctx.model.Log.find()
-      .skip(limit * (page - 1)).limit(limit).sort({ _id: -1 })
+    // 或-$or
+    let filter = {
+      $or: [
+        { 'req.value': { $gte: 0.6 } },
+        { 'res.value': { $gte: 0.6 } },
+        { 'ext.value': { $gte: 0.6 } }
+      ]
+    }
+    // 与-默认
+    filter = {
+      'req.value': { $gte: 0.6 },
+      'res.value': { $gte: 0.6 },
+      'ext.value': { $gte: 0.6 }
+    }
+    // 与-默认
+    filter = {
+      // 效率低下 - 慎用 - 此案例可以通过存成字符串配合正则$regex实现
+      $where: function () {
+        // 待续.... 得不到入参
+        const reqFilter = String(this.req.value).match(new RegExp("55"))
+        // const resFilter = String(this.req.value).match(new RegExp(query.res))
+        // const extFilter = String(this.req.value).match(new RegExp(query.ext))
+        // return reqFilter && resFilter && extFilter
+        return reqFilter
+      }
+    }
+    const count = await this.ctx.model.Log.count(filter)
+    const list = await this.ctx.model.Log
+      .find(filter).skip(limit * (page - 1))
+      .limit(limit).sort({ _id: -1 })
     return {
       list, // 数据集合
       page, // 当前是第几页
